7c3c81f587ff5629ffab56faa6b9f7d97570b241
wooth4t <69492134+wooth4t@users.noreply.github.com>
2021-02-24 19:04:20 -0500

    updated default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather defaultCity="New York"/>
+      <Weather defaultCity="Toronto"/>
       </div>
        
     </div>
